a9fd3ff8a9306ccdba4560252cd9cd8a54dd2546
sujal <sujaltank12@gmail.com>
2024-10-30 13:31:07 +0530

    savee

--- jobindiajob/backend/server.js
+++ jobindiajob/backend/server.js
@@ -5,7 +5,7 @@ const cookieparser = require('cookie-parser')
 const cors = require('cors')
 const path = require('path')
 const app = express()
-const port = 3000
+const port = 4000
 dotenv.config()
 //db connect
 Connectdb()
